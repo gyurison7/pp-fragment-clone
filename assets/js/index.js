1808f915c840bfe0ca0b43900a3f3a1b0176142a
gyurison7 <orangetree401@gmail.com>
2024-02-21 01:57:27 +0900

    feat: 모바일 사이즈 반응형 작업

--- assets/js/index.js
+++ assets/js/index.js
@@ -40,43 +40,40 @@ introMotion
     $("body").removeClass("hidden");
   }, "b");
 
-function sideBarOpen() {
+function sideMenuOpen() {
   gsap.to(".side-bar", { x: "35.5vw", duration: 0.5, ease: "linear" });
+  gsap.from(".side-menu .title, .side-menu .top-area a", {
+    yPercent: 100,
+    stagger: 0.1,
+  });
 }
-function sideBarClose() {
+function sideMenuClose() {
   gsap.to(".side-bar", { x: 0, duration: 0.5, ease: "linear" });
 }
 
 $(".toggle-btn").click(function () {
-  $(".side-menu, .side-bar").toggleClass("open");
-  if ($(".side-bar").hasClass("open")) {
-    sideBarOpen();
-  } else {
-    sideBarClose();
-  }
+  $(this).toggleClass("open");
+  $(".side-menu").toggleClass("open");
 
-  if ($(".side-menu").hasClass("open") && $(".side-bar").hasClass("open")) {
-    gsap.from(".side-menu .title, .side-menu .top-area a", {
-      yPercent: 100,
-      stagger: 0.1,
-    });
+  if ($(this).hasClass("open") && $(".side-menu").hasClass("open")) {
+    sideMenuOpen();
+  } else {
+    sideMenuClose();
   }
 });
 
 $(".side-menu a").click(function () {
-  $(".side-menu, .side-bar").removeClass("open");
-  sideBarClose();
+  $(".side-menu, .toggle-btn").removeClass("open");
+  sideMenuClose();
 });
 
 $(document).click(function (e) {
   if (
-    $(".side-menu").hasClass("open") &&
-    $(".side-bar").hasClass("open") &&
-    !$(e.target).closest(".side-menu").length &&
-    !$(e.target).closest(".side-bar").length
+    $(".side-menu, .toggle-btn").hasClass("open") &&
+    !$(e.target).closest(".side-menu, .toggle-btn").length
   ) {
-    $(".side-menu, .side-bar").removeClass("open");
-    sideBarClose();
+    $(".side-menu, .toggle-btn").removeClass("open");
+    sideMenuClose();
   }
 });
 
@@ -91,7 +88,8 @@ function dynamicWrapLines() {
     element.innerHTML = "";
 
     wordArray.forEach((word) => {
-      const testLine = line + word + " "; // 가로너비 비교를 위한 테스트 라인 생성
+      const trimWord = word.replace(/\n/g, " "); // 줄바꿈 문자 공백으로 대체
+      const testLine = line + trimWord + " "; // 가로너비 비교를 위한 테스트 라인 생성
       const tempSpan = document.createElement("span"); // 임시 span 생성
       tempSpan.style.position = "absolute";
       tempSpan.style.left = "-9999px";
@@ -108,7 +106,7 @@ function dynamicWrapLines() {
         newLine.innerText = line; // line에는 현재 단어 이전까지의 텍스트가 담겨 있음
         element.appendChild(animationWrapper); // 해당 요소에 부모 wrapper를 먼저 추가
         animationWrapper.appendChild(newLine); // 최종적으로 부모 wrapper에 라인을 자식으로 추가
-        line = word + " "; // 라인에 현재 단어를 넣어서 초기화
+        line = trimWord + " "; // 라인에 현재 단어를 넣어서 초기화
       } else {
         line = testLine;
       }
@@ -132,7 +130,20 @@ window.addEventListener("load", function () {
   dynamicAnimation1();
   dynamicAnimation2();
 });
-window.addEventListener("resize", dynamicWrapLines);
+
+function debounce(func, wait) {
+  let timeout; // 타이머를 저장할 변수
+  return function () {
+    clearTimeout(timeout); // 이전 타이머가 있으면 취소
+    timeout = setTimeout(func, wait); // 새로운 타이머 설정
+  };
+}
+
+const debouncedDynamicWrapLines = debounce(() => {
+  dynamicWrapLines();
+}, 300);
+
+window.addEventListener("resize", debouncedDynamicWrapLines);
 
 // sc-intro
 const paragraphMotion1 = gsap.timeline({
@@ -140,7 +151,6 @@ const paragraphMotion1 = gsap.timeline({
     trigger: ".sc-intro .top-section",
     start: "0% 100%",
     end: "100% 50%",
-    // scrub: true
     // markers: true,
   },
 });
@@ -152,18 +162,27 @@ paragraphMotion1
 function dynamicAnimation1() {
   const paragraphMotion2 = gsap.timeline({
     scrollTrigger: {
-      trigger: ".sc-intro .bottom-section",
+      trigger: ".sc-intro .right-area",
       start: "0% 50%",
       end: "100% 50%",
-      // scrub: true,
       // markers: true,
     },
   });
   paragraphMotion2
-    .from(".sc-intro .bottom-section .description1 .line", { yPercent: 100, stagger: 0.1 }, "a")
-    .from(".sc-intro .bottom-section .description2 .line", { yPercent: 100, stagger: 0.1 }, "a")
-    .from(".sc-intro .border-animation", { width: 0, stagger: 0.1 }, "a+=0.5");
+    .from(".sc-intro .right-area .description1 .line", { yPercent: 100, stagger: 0.1 }, "a")
+    .from(".sc-intro .right-area .description2 .line", { yPercent: 100, stagger: 0.1 }, "a");
 }
+
+gsap.to(".sc-intro .table tr", {
+  scrollTrigger: {
+    trigger: ".sc-intro .table",
+    start: "0% 100%",
+    end: "100% 100%",
+    // markers: true,
+  },
+  "--border-width": "100%",
+  stagger: 0.1,
+});
 
 // sc-style
 const gatherMotion = gsap.timeline({
@@ -293,10 +312,11 @@ window.addEventListener("load", function () {
   changeAnimation(".p8", [30, 0, 60, 30], [400, 300, 700, 700]);
 });
 
-//sc-glyph
+//sc-glyph, sc-font
 // 메뉴 열기
-$(".sc-glyph .dropdown-btn").click(function () {
-  $(".sc-glyph .dropdown-btn, .sc-glyph .dropdown-list").toggleClass("open");
+$(".dropdown-btn").click(function () {
+  $(this).toggleClass("open");
+  $(this).siblings(".dropdown-list").toggleClass("open");
 });
 
 // 다른 부분 클릭 시 메뉴 닫기
@@ -341,13 +361,17 @@ $(".sc-font .font-list li").click(function () {
   const fontStyle = $(this).text();
   $(this).addClass("active").siblings().removeClass("active");
   $(".sc-font textarea").css("font-family", `var(--font-family-${fontStyle})`);
+
+  // mobile
+  $(this).closest("div").find(".dropdown-btn").text(fontStyle).removeClass("open");
+  $(this).closest(".dropdown-list").removeClass("open");
 });
 
 // font-setting
 $(".sc-font .input-range").on("input", function () {
   const value = $(this).val();
   const parentClass = $(this).closest("div").attr("class");
-  const fontSize = parentClass === "font-size" ? value : 240;
+  const fontSize = parentClass === "font-size" ? value : window.innerWidth > 699 ? 240 : 100;
 
   switch (parentClass) {
     case "font-size":
@@ -365,12 +389,13 @@ $(".sc-font .input-range").on("input", function () {
     case "line-height":
       $(".line-height .value").text(`${value}pt`);
       $(".sc-font textarea").css("line-height", `${value / fontSize}`);
+      $(".sc-font textarea").css("--line-height", `${value / fontSize}`);
       break;
   }
 });
 
 // sc-poster
-const bgSwiper = new Swiper(".bg-swiper-container", {
+const bgSwiper = new Swiper(".bg-swiper", {
   direction: "vertical",
   slidesPerView: 1,
   spaceBetween: 20,
@@ -384,7 +409,7 @@ const bgSwiper = new Swiper(".bg-swiper-container", {
     disableOnInteraction: false, // 사용자 상호작용 후에도 자동 슬라이드 유지
   },
 });
-const posterSwiper = new Swiper(".poster-swiper-container", {
+const posterSwiper = new Swiper(".poster-swiper", {
   direction: "vertical",
   slidesPerView: 1,
   spaceBetween: 20,
@@ -408,24 +433,6 @@ const posterSwiper = new Swiper(".poster-swiper-container", {
     disableOnInteraction: false,
   },
 });
-
-// sc-language
-// const gatherLettersMotion = gsap.timeline({
-//   scrollTrigger: {
-//     trigger: ".sc-language",
-//     start: "0% 80%",
-//     end: "80% 0%",
-//     scrub: true,
-//     // markers: true,
-//   },
-// });
-// gatherLettersMotion
-//   .from(".gather-letters .l1", { xPercent: -20, yPercent: 20, opacity: 0 }, "a")
-//   .from(".gather-letters .l2", { xPercent: 20, yPercent: 0, opacity: 0 }, "a")
-//   .from(".gather-letters .l3", { xPercent: 0, yPercent: 15, scale: 1.2, opacity: 0 }, "a+=0.1")
-//   .from(".gather-letters .l4", { xPercent: -20, yPercent: 15, opacity: 0 }, "a+=0.1")
-//   .from(".gather-letters .l5", { xPercent: 0, yPercent: 50, opacity: 0 }, "a+=0.1")
-//   .from(".gather-letters .l6", { xPercent: 20, yPercent: 0, opacity: 0 }, "a+=0.1");
 
 // sc-language
 gsap.set(".gather-letters .l1", { xPercent: -20, yPercent: 20, opacity: 0 });
@@ -456,7 +463,6 @@ const languageMotion = gsap.timeline({
     trigger: ".sc-language .language-list",
     start: "0% 90%",
     end: "100% 30%",
-    scrub: true,
     // markers: true,
   },
 });
@@ -524,17 +530,18 @@ function dynamicAnimation2() {
     scrollTrigger: {
       trigger: ".sc-credits",
       start: "0% 70%",
-      end: "100% 50%",
+      end: "100% 100%",
       // scrub: true,
       // markers: true,
     },
   });
   paragraphMotion3
-    .from(".sc-credits .border-animation", { width: 0 }, "a")
+    .to(".sc-credits h3", { "--border-width": "100%" }, "a")
     .from(".sc-credits .p-col1 .line", { yPercent: 100, stagger: 0.1 }, "a+=0.1")
     .from(".sc-credits .p-col2 .line", { yPercent: 100, stagger: 0.1 }, "a+=0.1")
     .from(".sc-credits .p-col3 .line", { yPercent: 100, stagger: 0.1 }, "a+=0.1")
-    .from(".sc-credits .p-col4 .line", { yPercent: 100, stagger: 0.1 }, "a+=0.1");
+    .from(".sc-credits .p-col4 .line", { yPercent: 100, stagger: 0.1 }, "a+=0.1")
+    .from(".sc-credits .visuals-section .artist", { yPercent: 100 }, "a+=0.2");
 }
 
 // footer
